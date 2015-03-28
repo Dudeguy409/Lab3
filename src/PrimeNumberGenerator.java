31fe662d5f0b52bdd684e21cc543b99babbaa7b1
Andrew Davidson <acdavidson@sbcglobal.net>
2015-03-27 23:24:23 -0400

    got second test working

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -5,7 +5,9 @@ public class PrimeNumberGenerator {
 
 	public List<Integer> generate(Integer inputNumber) {
 		ArrayList<Integer> rslt = new ArrayList<Integer>();
-		
+		if (inputNumber > 2) {
+			rslt.add(2);
+		}
 		return rslt;
 	}
 
