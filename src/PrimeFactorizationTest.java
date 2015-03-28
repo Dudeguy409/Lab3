6e9b49b8d5e1ef9f315189a875f22e67ff8cfcb3
Andrew Davidson <acdavidson@sbcglobal.net>
2015-03-27 22:19:28 -0400

    created project

---
+++ src/PrimeFactorizationTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+
+public class PrimeFactorizationTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
