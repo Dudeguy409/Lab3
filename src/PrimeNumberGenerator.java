c37d7307355153104445a3fc98ca514adc105ae0
Andrew Davidson <acdavidson@sbcglobal.net>
2015-03-27 23:19:52 -0400

    created primeNumberGenerator class and test class, and wrote first test case

---
+++ src/PrimeNumberGenerator.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+
+public class PrimeNumberGenerator {
+
+	public List<Integer> generate(Integer inputNumber) {
+		return null;
+	}
+
+}
---
+++ src/PrimeNumberGeneratorTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class PrimeNumberGeneratorTest {
+	private PrimeNumberGenerator primeNumberGenerator;
+	private Integer inputNumber;
+	private List<Integer> expectedResult;
+
+	@Before
+	public void initialize() {
+		primeNumberGenerator = new PrimeNumberGenerator();
+	}
+
+	public PrimeNumberGeneratorTest(Integer inputNumber,
+			List<Integer> expectedResult) {
+		this.inputNumber = inputNumber;
+		this.expectedResult = expectedResult;
+	}
+
+	@Parameterized.Parameters
+	public static Collection primeNumbers() {
+		return Arrays.asList(new Object[][] { { 2, list() } });
+	}
+
+	private static List<Integer> list(Integer... integers) {
+		return Arrays.asList(integers);
+	}
+
+	  @Test
+	   public void testPrimeNumberGenerator() {
+	      System.out.println("Parameterized Number is : " + inputNumber);
+	      assertEquals(expectedResult, 
+	      primeNumberGenerator.generate(inputNumber));
+	   }
+
+}
